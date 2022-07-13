2ca05ec728916c3ef766b34519365049a4dc7bc9
Michal-W-dev <maikey20@gmail.com>
2022-07-13 18:16:16 +0200

    Update React

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import { HashRouter as Router } from 'react-router-dom';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
 
-ReactDOM.render(
-  <Router basename='/'>
-    <App />
-  </Router>,
-  document.getElementById('root')
-)
+createRoot(document.getElementById('root')!)
+  .render(
+    <Router basename='/'>
+      <App />
+    </Router>
+  );
